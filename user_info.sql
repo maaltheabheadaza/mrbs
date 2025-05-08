9ed54d61d7a9b92ad85e916077609acb375c3dc8
cha-rl-es <tirzocharles.apuya@evsu.edu.ph>
2025-05-08 19:18:22 +0800

    ALTER TABLE booking_preferences1 ADD per_hour INT; (add lang ni)

--- user_info.sql
+++ user_info.sql
@@ -6,7 +6,7 @@
 -- Generation Time: May 08, 2025 at 01:14 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
-
+--
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
