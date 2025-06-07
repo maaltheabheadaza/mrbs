db21b935acb4df422c683c1a87527bbf1da1ce38
MackieRizz <147297637+MackieRizz@users.noreply.github.com>
2025-06-07 17:56:47 +0800

    loginsucess

--- Php_Codes/admin_login.php
+++ Php_Codes/admin_login.php
@@ -16,15 +16,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM admin WHERE email='$email' AND valid_password='$password'";
+    $sql = "SELECT * FROM admin WHERE email='$email'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
         $adminData = $result->fetch_assoc();
-        $_SESSION['admin'] = $adminData;
-        $_SESSION['admin_name'] = $adminData['name'];
-        header("Location: ../Php_Codes/AdminPage_dashboard.php?login_success=1");
-        exit();
+        if (password_verify($password, $adminData['valid_password'])) {
+            $_SESSION['admin'] = $adminData;
+            $_SESSION['admin_name'] = $adminData['fullname'];
+            header("Location: ../Php_Codes/AdminPage_dashboard.php?login_success=1");
+            exit();
+        } else {
+            echo '<script>alert("Incorrect email or password. Please try again.");</script>';
+            echo '<script>window.location.href = "../Html_Codes/adminlogin.html";</script>';
+        }
     } else {
         echo '<script>alert("Incorrect email or password. Please try again.");</script>';
         echo '<script>window.location.href = "../Html_Codes/adminlogin.html";</script>';
